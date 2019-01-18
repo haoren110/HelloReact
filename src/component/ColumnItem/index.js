f5cb884847b537e5bbc326df7e7544ef08b590c8
haoren110 <1030936082@qq.com>
2019-01-18 16:47:48 +0800

    入口item

--- src/component/ColumnItem/index.js
+++ src/component/ColumnItem/index.js
@@ -4,12 +4,22 @@ class ColumItem extends  React.Component{
         super(props);
     }
     render() {
-        return (<li className="f_left"><a href={this.props.data.url}>
-            <div className="aplicon"><img src={this.props.data.icon} alt="" />
-                <div className="unnotice">{this.props.data.num}</div>
-            </div>
-            <span>{this.props.data.text}</span>
-        </a></li>);
+        console.log(this.props.data.num)
+        if(this.props.data.num==false){
+            return (<li className="f_left"><a href={this.props.data.url}>
+                <div className="aplicon"><img src={this.props.data.icon} alt="" />
+                    <div className="unnotice">{this.props.data.num}</div>
+                </div>
+                <span>{this.props.data.text}</span>
+            </a></li>);
+        }else {
+            return (<li className="f_left"><a href={this.props.data.url}>
+                <div className="aplicon"><img src={this.props.data.icon} alt="" />
+                </div>
+                <span>{this.props.data.text}</span>
+            </a></li>);
+        }
+
     }
 
 }
