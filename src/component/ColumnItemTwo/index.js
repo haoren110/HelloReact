d1346fac00f0abf677f8ff6e3c9358faa36ee99b
haoren110 <1030936082@qq.com>
2019-01-18 17:26:10 +0800

    入口itemTwo

---
+++ src/component/ColumnItemTwo/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const baseurl="https://weixin.buchang.com/ZbtOfficial/"
+class ColumItemTwo extends  React.Component{
+    constructor(props){
+        super(props);
+    }
+    render() {
+          let url=baseurl+this.props.data.iconUrl;
+            return (<li className="f_left"><a href={this.props.data.url}>
+                <div className="aplicon"><img src={this.props.data.icon} alt="" />
+                </div>
+                <span>{this.props.data.name}</span>
+            </a></li>);
+
+
+    }
+
+}
+
+export default ColumItemTwo;
